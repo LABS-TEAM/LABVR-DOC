07e46f095b2763f0315bd65bd1d499f3fd6d4158
Alexandre Figard <alexandre@figard.fr>
2024-01-18 09:02:42 +0100

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm run build
 
 git branch -D gh-pages
 git push origin --delete gh-pages
-git branch gh-pages
+git checkout -b gh-pages
 rm -rf ./*
 
 mv build/* .
@@ -11,4 +11,8 @@ git add .
 git commit -m "deploy $(date +%Y-%m-%d:%H:%M:%S)"
 git push origin gh-pages -f
 
+rm -rf ./build
 git checkout main
+git add .
+git commit -m "clean build"
+git push origin main
