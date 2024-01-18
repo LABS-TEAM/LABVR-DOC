7167b87dbcb764babb67125b9ef20f16da7a3a70
Alexandre Figard <alexandre@figard.fr>
2024-01-18 08:51:21 +0100

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 npm install
 npm run build
 
-
-git branch -D gh-pages 
 git push origin --delete gh-pages
-git checkout --orphan gh-pages
+git branch gh-pages
 
 git add build/* 
 git commit -m "deploy $(date +%Y-%m-%d:%H:%M:%S)"
 git push origin gh-pages -f
+
+git checkout main
