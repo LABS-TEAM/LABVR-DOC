cb6091bda8701d0a6914f459b5e6be8f17fde1b6
Alexandre Figard <alexandre@figard.fr>
2024-01-18 10:26:36 +0100

    add good url base

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "http://labs-team.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/LABVR-DOC",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
