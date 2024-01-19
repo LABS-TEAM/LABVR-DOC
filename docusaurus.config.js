4061c3de5d3bdf1a47caa30c9c9cbf8dfc04b8be
Alexandre Figard <alexandre@figard.fr>
2024-01-19 15:10:18 +0100

    update url documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
             items: [
               {
                 label: "Documentation",
-                to: "/docs/intro",
+                to: "/docs/docs/intro",
               },
             ],
           },
